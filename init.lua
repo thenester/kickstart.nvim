e00a6e803be3f9c9c4b670b779e062578ba5d804
Denys (thenester) Nesterenko <denys@thenester.co>
2023-09-30 21:57:03 +0300

    Change default theme
    Signed-off-by: Denys (thenester) Nesterenko <denys@thenester.co>

--- init.lua
+++ init.lua
@@ -153,11 +153,11 @@ require('lazy').setup({
   },
 
   {
-    -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
+    -- Theme
+    'savq/melange-nvim',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'onedark'
+      vim.cmd.colorscheme 'melange'
     end,
   },
 
@@ -168,7 +168,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = true,
-        theme = 'onedark',
+        theme = 'auto',
         component_separators = '|',
         section_separators = { left = '', right = ''},
       },

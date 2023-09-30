7acd490bbd570fe7ff798121a47993ef2cabb5a6
Denys (thenester) Nesterenko <denys@thenester.co>
2023-09-30 21:57:34 +0300

    Add autoformat plugin
    Signed-off-by: Denys (thenester) Nesterenko <denys@thenester.co>

--- init.lua
+++ init.lua
@@ -88,7 +88,7 @@ require('lazy').setup({
 
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
+      { 'j-hui/fidget.nvim',       tag = 'legacy', opts = {} },
 
       -- Additional lua configuration, makes nvim stuff amazing!
       'folke/neodev.nvim',
@@ -112,7 +112,7 @@ require('lazy').setup({
   },
 
   -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim', opts = {} },
+  { 'folke/which-key.nvim',  opts = {} },
   {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
@@ -170,7 +170,7 @@ require('lazy').setup({
         icons_enabled = true,
         theme = 'auto',
         component_separators = '|',
-        section_separators = { left = '', right = ''},
+        section_separators = { left = '', right = '' },
       },
       sections = {
         lualine_z = {
@@ -228,8 +228,8 @@ require('lazy').setup({
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
-  -- require 'kickstart.plugins.autoformat',
   -- require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.autoformat',
   require 'custom.plugins.autopairs',
   require 'custom.plugins.lazygit'
 

18890eb4b5345436b57fc2096fcab03cbb56a843
Denys (thenester) Nesterenko <denys@thenester.co>
2023-10-01 18:26:13 +0300

    Add Lazy plugin keybinding
    Signed-off-by: Denys (thenester) Nesterenko <denys@thenester.co>

--- init.lua
+++ init.lua
@@ -360,6 +360,7 @@ vim.keymap.set('n', '<leader>/', function()
 end, { desc = '[/] Fuzzily search in current buffer' })
 
 vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
+vim.keymap.set('n', '<leader>l', '<Cmd>Lazy<CR>', { desc = 'Run [L]azy' })
 vim.keymap.set('n', '<leader>gg', '<Cmd>LazyGit<CR>', { desc = 'Run Lazy[G]it' })
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })

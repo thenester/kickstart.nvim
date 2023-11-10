f0216963511db31e174fc2d25e19e51268a2a0ce
Denys (thenester) Nesterenko <denys@thenester.co>
2023-11-10 18:08:42 +0200

    Remove not needed languages

--- init.lua
+++ init.lua
@@ -398,8 +398,7 @@ vim.keymap.set('n', '<Tab>n', '<Cmd>tabnew<CR>', { desc = '[T]ab [C]reate' })
 vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim',
-      'bash' },
+    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'vim', 'bash' },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
